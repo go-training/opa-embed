f4dbc1c3665a62d9b7678a4bf2caab3d89fc3c17
Bo-Yi Wu <appleboy.tw@gmail.com>
2021-04-18 10:01:37 +0800

    chore: initial rego testing

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/go-training/opa-demo
+
+go 1.16
+
+require github.com/open-policy-agent/opa v0.27.1
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+	"log"
+
+	"github.com/open-policy-agent/opa/rego"
+)
+
+//go:embed example.rego
+var policy []byte
+var policyFile = "example.rego"
+var defaultQuery = "x = data.rbac.authz.allow"
+
+type input struct {
+	User   string `json:"user"`
+	Action string `json:"action"`
+	Object string `json:"object"`
+}
+
+func main() {
+	s := input{
+		User:   "design_group_kpi_editor",
+		Action: "view_all",
+		Object: "design",
+	}
+
+	input := map[string]interface{}{
+		"user":   s.User,
+		"action": s.Action,
+		"object": s.Object,
+	}
+
+	ctx := context.TODO()
+	query, err := rego.New(
+		rego.Query(defaultQuery),
+		rego.Module(policyFile, string(policy)),
+	).PrepareForEval(ctx)
+
+	if err != nil {
+		log.Fatalf("initial rego error: %v", err)
+	}
+
+	results, err := query.Eval(ctx, rego.EvalInput(input))
+	if err != nil {
+		log.Fatalf("evaluation error: %v", err)
+	} else if len(results) == 0 {
+		log.Fatal("undefined result", err)
+		// Handle undefined result.
+	} else if result, ok := results[0].Bindings["x"].(bool); !ok {
+		log.Fatalf("unexpected result type: %v", result)
+	} else {
+		// Handle result/decision.
+		fmt.Printf("%+v", results)
+	}
+}
